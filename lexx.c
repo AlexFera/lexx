15b48bfc88d40c0ede859114061e5f25fe6ca6e3
alexfera <alexandrufera@gmail.com>
2010-12-13 16:32:22 +0200

    fixed a typo

--- lexx.c
+++ lexx.c
@@ -21,9 +21,9 @@
 #include <string.h>
 
 /* Analiză lexicală manuală */
-void lexical_analysis(FILE *input_file, char current_charcter, FILE *output_file)
+void lexical_analysis(FILE *input_file, char current_character, FILE *output_file)
 {
-	char token_charcters[256];		/* tablou de carctere unde sunt depuse dupa *
+	char token_characters[256];		/* tablou de carctere unde sunt depuse dupa *
        						* caz caracterele componente ale atomului curent*/
 	unsigned int  length = 0;		/* variabilă contor */
 	unsigned int  i = 0;			/* variabilă contor */
@@ -37,53 +37,53 @@ void lexical_analysis(FILE *input_file, char current_charcter, FILE *output_file
 	/* Parcurgere cod sursă până la întâlnirea
 	 * unui carcter diferit de spatiu alb */
 	is_keyword = 0;
-	while(current_charcter == ' ' || current_charcter == '\n')
-		current_charcter = fgetc(input_file);
+	while(current_character == ' ' || current_character == '\n')
+		current_character = fgetc(input_file);
 
 	/* Clasificare carcter si prelucrare atom */
-	if(isalpha(current_charcter)) {
+	if(isalpha(current_character)) {
 		/* Prelucrează identificator sau cuvânt cheie */
 		
-		while(isalpha(current_charcter) || isdigit(current_charcter)) {
-			token_charcters[length++] = current_charcter;
-			current_charcter = fgetc(input_file);
+		while(isalpha(current_character) || isdigit(current_character)) {
+			token_characters[length++] = current_character;
+			current_character = fgetc(input_file);
 		}
-		token_charcters[length++] = '\0';
+		token_characters[length++] = '\0';
 		/* testăm dacă este cuvânt cheie */
 		for(i = 0; i < 13; i++) {
-			if(strcmp(keywords[i], token_charcters) == 0) {
-				fprintf(output_file, "%s \t\t\teste cuvant cheie\n", token_charcters);
+			if(strcmp(keywords[i], token_characters) == 0) {
+				fprintf(output_file, "%s \t\t\teste cuvant cheie\n", token_characters);
 				is_keyword = 1;
 				break;
 			}
 		}
 		if(is_keyword == 0) 
-			fprintf(output_file, "%s \t\t\teste idenfificator\n", token_charcters);
+			fprintf(output_file, "%s \t\t\teste idenfificator\n", token_characters);
 	}
 	else
-		if(isdigit(current_charcter)) {
+		if(isdigit(current_character)) {
 			/* Prelucrează constantă numerică */
-			while(isdigit(current_charcter)) {
-				number = number * 10 + atoi(&current_charcter);
-				current_charcter = fgetc(input_file);
+			while(isdigit(current_character)) {
+				number = number * 10 + atoi(&current_character);
+				current_character = fgetc(input_file);
 			}
 			fprintf(output_file, "%d \t\t\teste un numar\n", number);
 		}
 		else
 			/* Altfel este operator */
 			for(i = 0; i < 14; i++)
-				if(current_charcter == operators[i]) 
-					fprintf(output_file, "%c \t\t\teste un operator\n", current_charcter);
+				if(current_character == operators[i]) 
+					fprintf(output_file, "%c \t\t\teste un operator\n", current_character);
 	for(i = 0; i < 10; i++)
-		if(current_charcter == separators[i]) {
-			fprintf(output_file, "%c \t\t\teste un semn de punctuatie\n", current_charcter);
+		if(current_character == separators[i]) {
+			fprintf(output_file, "%c \t\t\teste un semn de punctuatie\n", current_character);
 			break;
 		}
-	if(current_charcter == '"') {
+	if(current_character == '"') {
 		do {
-			current_charcter = fgetc(input_file);
-			fprintf(output_file, "%c", current_charcter);
-		}while(current_charcter != '"');
+			current_character = fgetc(input_file);
+			fprintf(output_file, "%c", current_character);
+		}while(current_character != '"');
 		fprintf(output_file, "\t\teste un sir de caractere\n");
 	}
 }
@@ -93,7 +93,7 @@ int main(int argc, char **argv)
 	const char *filename;
 	FILE *input_file;
 	FILE *output_file;
-	char current_charcter;
+	char current_character;
 
 	if(argc == 1) {
 		printf("Introduceti numele fisierului ca parametru in " 
@@ -113,8 +113,8 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 	
-	while((current_charcter = fgetc(input_file)) != EOF)
-		lexical_analysis(input_file, current_charcter, output_file);
+	while((current_character = fgetc(input_file)) != EOF)
+		lexical_analysis(input_file, current_character, output_file);
 	
 	fclose(input_file);
 	fclose(output_file);
