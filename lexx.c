503cce7a3404efde80e30dac316cebd7db2044ae
alexfera <alexandrufera@gmail.com>
2011-01-19 18:38:40 +0200

    Updated copyright notice

--- lexx.c
+++ lexx.c
@@ -1,5 +1,5 @@
 /* 
- *  Copyright (C) 2010 <alexnadrufera at gmail dot com>
+ *  Copyright (C) 2011 <alexnadrufera at gmail dot com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
